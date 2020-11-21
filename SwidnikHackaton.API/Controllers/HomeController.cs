51dc6212c5da07442d87df255cd4198b79523a39
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-11-21 13:55:17 +0100

    Add project files.

---
+++ SwidnikHackaton.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwidnikHackaton.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "I'm Swidnik API and current time is: " + DateTime.Now;
+        }
+    }
+}
---
+++ SwidnikHackaton.API/Controllers/StreetsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwidnikHackaton.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StreetsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Street> Get()
+        {
+            return new List<Street>()
+            {
+                new Street() {StreetName = "sdadsa", Latitude = (float)123.123, Longitude = (float)123.123},
+                new Street() {StreetName = "sdadsa1", Latitude = (float)123.123, Longitude = (float)123.123},
+                new Street() {StreetName = "sdadsa2", Latitude = (float)123.123, Longitude = (float)123.123},
+            };
+        }
+    }
+
+    public class Street
+    {
+        public string StreetName { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
+
+
